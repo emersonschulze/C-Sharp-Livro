c4fc018b1a5c4f2a3a1f2a255a17753b897cee06
Bárbara Oliveira da Silva <barbara.silva33@fatec.sp.gov.br>
2017-12-06 15:44:24 -0200

    Add files via upload

---
+++ Capitulo11/AcerteATopeira/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace AcerteATopeira
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 87);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(131, 118);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 87);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(230, 26);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(103, 87);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(337, 118);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(103, 87);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(444, 26);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(103, 87);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 228);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Acerte-a-toupeira";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Capitulo11/AcerteATopeira/Mole.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace AcerteATopeira
+{
+    class Mole
+    {
+        public delegate void PopUp(int hole, bool show);
+        private PopUp popUpCalback;
+        private bool hidden;
+        public bool Hidden { get { return hidden; } }
+        private int timesHit = 0;
+        private int timesShown = 0;
+        private int hole = 0;
+        Random random;
+
+
+        public Mole(Random random, PopUp popUpCallback)
+        {
+            if(popUpCalback == null)
+            {
+                throw new ArgumentException("popUpCallBack can't be null");
+            }
+            else
+            {
+                this.random = random;
+                this.popUpCalback = popUpCallback;
+                hidden = true;
+            }
+
+            
+        }
+
+        public void Show()
+        {
+            timesShown++;
+            hidden = false;
+            hole = random.Next(5);
+            popUpCalback(hole, true);
+        }
+
+        public void HideAgain()
+        {
+            hidden = true;
+            popUpCalback(hole, false);
+            CheckForGameOver();
+        }
+
+        public void Smacked(int holeSmacked)
+        {
+            if(holeSmacked == hole)
+            {
+                timesHit++;
+                hidden = true;
+                CheckForGameOver();
+                popUpCalback(hole, false);
+            }
+        }
+
+        private void CheckForGameOver()
+        {
+            if(timesShown >= 10)
+            {
+                popUpCalback(-1, false);
+                System.Windows.Forms.MessageBox.Show("Você acertou: "+timesHit+"\nGame Over :(");
+                Application.Exit();
+            }
+        }
+    }
+}
