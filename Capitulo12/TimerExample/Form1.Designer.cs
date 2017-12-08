7bce9c69d6e4ff6db4bb721d4cdf5bbc7aa95d4e
Bárbara Oliveira da Silva <barbara.silva33@fatec.sp.gov.br>
2017-12-08 21:08:44 -0200

    Add files via upload

---
+++ Capitulo12/TimerExample/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace TimerExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnAtivo = new System.Windows.Forms.Button();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.btnParar = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnAtivo
+            // 
+            this.btnAtivo.Location = new System.Drawing.Point(34, 35);
+            this.btnAtivo.Name = "btnAtivo";
+            this.btnAtivo.Size = new System.Drawing.Size(206, 35);
+            this.btnAtivo.TabIndex = 0;
+            this.btnAtivo.Text = "Ativo";
+            this.btnAtivo.UseVisualStyleBackColor = true;
+            this.btnAtivo.Click += new System.EventHandler(this.btnAtivo_Click);
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(34, 115);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(206, 35);
+            this.btnIniciar.TabIndex = 1;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // btnParar
+            // 
+            this.btnParar.Location = new System.Drawing.Point(34, 194);
+            this.btnParar.Name = "btnParar";
+            this.btnParar.Size = new System.Drawing.Size(206, 35);
+            this.btnParar.TabIndex = 2;
+            this.btnParar.Text = "Parar";
+            this.btnParar.UseVisualStyleBackColor = true;
+            this.btnParar.Click += new System.EventHandler(this.btnParar_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnParar);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.btnAtivo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAtivo;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Button btnParar;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Capitulo12/TimerExample/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimerExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Console.WriteLine(DateTime.Now.ToString());
+        }
+
+        private void btnAtivo_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                timer1.Enabled = false;
+            }
+            else
+            {
+                timer1.Enabled = true;
+            }
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            Console.WriteLine("Enabled = "+timer1.Enabled);
+        }
+
+        private void btnParar_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            Console.WriteLine("Enable = " + timer1.Enabled);
+
+        }
+    }
+}
